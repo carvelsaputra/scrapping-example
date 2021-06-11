e176c9073b99e4872535015c40fbc99473229e2d
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-06-11 23:12:36 +0700

    init project and also adding 2 example simple scrapping

---
+++ belajar-scrapping/example.js
@@ -0,0 +1,23 @@
+const puppeteer = require("puppeteer");
+
+// ScreenShoot Website
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto("https://example.com");
+//   await page.screenshot({ path: "example.png" });
+
+//   await browser.close();
+// })();
+
+// Get Content on page
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const element = await page.waitForSelector("h1"); // select the element
+  const value = await element.evaluate((el) => el.textContent);
+  console.log(value);
+})();
