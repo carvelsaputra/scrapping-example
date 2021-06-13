968b25e7eeb037f0dde63384fcf1520c6631a6d9
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-06-13 17:08:13 +0700

    scrap content at a href
    WIP: get screenshot oncreate

--- example.js
+++ example.js
@@ -13,11 +13,22 @@ const puppeteer = require("puppeteer");
 
 // Get Content on page
 
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto("https://example.com");
+//   const element = await page.waitForSelector("h1"); // select the element
+//   const value = await element.evaluate((el) => el.textContent);
+//   console.log(value);
+// })();
+
+//Click URL and then get the content inside the href
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://example.com");
-  const element = await page.waitForSelector("h1"); // select the element
-  const value = await element.evaluate((el) => el.textContent);
-  console.log(value);
+  await page.click("a");
+  await page.screenshot({ path: "example.png" });
+  await browser.close();
 })();
